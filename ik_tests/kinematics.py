f8eba06648acf6592144a522d422d3b109d70563
Theo Kanning <theokanning@gmail.com>
2018-03-07 17:34:08 -0600

    Add kinematics and test scripts

---
+++ ik_tests/kinematics.py
@@ -0,0 +1,260 @@
+import numpy as np
+from sympy import symbols, cos, sin, pi, simplify, Transpose
+from math import atan2, sqrt, acos
+from sympy.matrices import Matrix
+
+# Symbols
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+# DH Parameters
+s = {alpha0:     0, a0:      0, d1:  0.75,
+     alpha1: -pi/2, a1:   0.35, d2:     0, q2: q2-pi/2,
+     alpha2:     0, a2:   1.25, d3:     0,
+     alpha3: -pi/2, a3: -0.054, d4:  1.50,
+     alpha4:  pi/2, a4:      0, d5:     0,
+     alpha5: -pi/2, a5:      0, d6:     0,
+     alpha6:     0, a6:      0, d7: 0.303, q7: 0}
+
+q = symbols('q')
+a = symbols('a')
+alpha = symbols('alpha')
+d = symbols('d')
+
+BASE_T = Matrix([[cos(q), -sin(q), 0, a],
+                 [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
+                 [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
+                 [0, 0, 0, 1]])
+
+T0_1 = BASE_T.subs({alpha: alpha0, a: a0, d: d1, q: q1}).subs(s)
+T1_2 = BASE_T.subs({alpha: alpha1, a: a1, d: d2, q: q2}).subs(s)
+T2_3 = BASE_T.subs({alpha: alpha2, a: a2, d: d3, q: q3}).subs(s)
+T3_4 = BASE_T.subs({alpha: alpha3, a: a3, d: d4, q: q4}).subs(s)
+T4_5 = BASE_T.subs({alpha: alpha4, a: a4, d: d5, q: q5}).subs(s)
+T5_6 = BASE_T.subs({alpha: alpha5, a: a5, d: d6, q: q6}).subs(s)
+T6_G = BASE_T.subs({alpha: alpha6, a: a6, d: d7, q: q7}).subs(s)
+
+# print("creating transforms")
+# T0_2 = simplify(T0_1 * T1_2)
+# print("T0_2", T0_2)
+# T0_3 = simplify(T0_2 * T2_3)
+# print("T0_3", T0_3)
+# T0_4 = simplify(T0_3 * T3_4)
+# print("T0_4", T0_4)
+# T0_5 = simplify(T0_4 * T4_5)
+# print("T0_5", T0_5)
+# T0_6 = simplify(T0_5 * T5_6)
+# print("T0_6", T0_6)
+# T0_G = simplify(T0_6 * T6_G)
+# print("T0_G", T0_G)
+# print("done")
+
+# Gripper link orientation correction
+R_z = Matrix([[cos(np.pi), -sin(np.pi), 0, 0],
+              [sin(np.pi), cos(np.pi), 0, 0],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1]])
+
+R_y = Matrix([[cos(-np.pi/2), 0, sin(-np.pi/2), 0],
+              [0, 1, 0, 0],
+              [-sin(-np.pi/2), 0, cos(-np.pi/2), 0],
+              [0, 0, 0, 1]])
+
+R_corr = R_z * R_y
+R_corr3 = R_corr[:3, :3]
+
+# print("T0_1 = ", T0_1.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+# print("T0_2 = ", T0_2.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+# print("T0_3 = ", T0_3.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+# print("T0_4 = ", T0_4.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+# print("T0_5 = ", T0_5.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+# print("T0_6 = ", T0_6.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+# print("T0_G = ", T0_G.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+
+# T_total = simplify(T0_G * R_corr)
+
+
+def get_forward(angles):
+    """
+    Calculates EE position and orientation given joint angles
+    returns ([x, y, z], [x, y, z, w])
+    """
+    transform = T0_1.evalf(subs={q1: angles[0]})
+    transform *= T1_2.evalf(subs={q2: angles[1]})
+    transform *= T2_3.evalf(subs={q3: angles[2]})
+    transform *= T3_4.evalf(subs={q4: angles[3]})
+    transform *= T4_5.evalf(subs={q5: angles[4]})
+    transform *= T5_6.evalf(subs={q6: angles[5]})
+    transform *= T6_G
+    transform *= R_corr.evalf()
+
+    rotation = angles_from_transform(transform)
+
+    x = transform[0, 3]
+    y = transform[1, 3]
+    z = transform[2, 3]
+
+    return [x, y, z], rotation
+
+
+def angles_from_transform(transform):
+    """
+    Returns the euler angles corresponding to the given 3x3 rotation matrix
+    :param transform: 4x4 Matrix object
+    :return: (roll, pitch, yaw)
+    """
+    matrix = transform.evalf()
+    r11 = matrix[0, 0]
+    r21 = matrix[1, 0]
+    r31 = matrix[2, 0]
+    r32 = matrix[2, 1]
+    r33 = matrix[2, 2]
+
+    yaw = atan2(r21, r11)
+    pitch = atan2(-r31, sqrt(r11**2 + r21**2))
+    roll = atan2(r32, r33)
+
+    return roll, pitch, yaw
+
+
+def wrist_angles_from_transform(transform):
+    """
+    Returns the necessary wrist joint angles to create the required transformation
+    :param transform: Matrix object
+    :return: theta4, theta5, theta6
+    """
+    matrix = transform.evalf()
+    r11 = matrix[0, 0]
+    r12 = matrix[0, 1]
+    r13 = matrix[0, 2]
+    r21 = matrix[1, 0]
+    r22 = matrix[1, 1]
+    r23 = matrix[1, 2]
+    r31 = matrix[2, 0]
+    r32 = matrix[2, 1]
+    r33 = matrix[2, 2]
+
+    # euler angles
+    # theta6 = atan2(r21, r11)
+    # theta5 = atan2(-r31, sqrt(r11 ** 2 + r21 ** 2))
+    # theta4 = atan2(r32, r33)
+
+    # calculated by me
+    theta4 = atan2(r33, -r13)
+    theta5 = atan2(sqrt(r22 ** 2 + r21 ** 2), r23)
+    theta6 = atan2(-r22, r21)
+
+    # from UMD class
+    # theta4 = atan2(r23, r13)
+    # theta5 = atan2(sqrt(r13 ** 2 + r23 ** 2), r33)
+    # theta6 = atan2(r32, -r31)
+
+    # From slack
+    # theta5 = atan2(sqrt(r13 ** 2 + r33 ** 2), r23)
+    # if sin(theta5) > 0:
+    #     theta4 = atan2(r33, -r13)
+    #     theta6 = atan2(-r22, r21)
+    # elif theta5 == 0:
+    #     theta4 = 0
+    #     theta6 = atan2(-r12, -r32)
+    # else:
+    #     theta4 = atan2(-r33, r13)
+    #     theta6 = atan2(r22, -r21)
+
+    return theta4, theta5, theta6
+
+
+def create_rotation_matrix(angles):
+    """
+    Returns a rotation matrix that will produce the given Euler angles
+    :param angles: (roll, pitch, yaw)
+    """
+    R_x = Matrix([[1, 0, 0],
+                  [0, cos(q), -sin(q)],
+                  [0, sin(q), cos(q)]]).evalf(subs={q: angles[0]})
+
+    R_y = Matrix([[cos(q), 0, sin(q)],
+                  [0, 1, 0],
+                  [-sin(q), 0, cos(q)]]).evalf(subs={q: angles[1]})
+
+    R_z = Matrix([[cos(q), -sin(q), 0],
+                  [sin(q), cos(q), 0],
+                  [0, 0, 1]]).evalf(subs={q: angles[2]})
+
+    return R_z * R_y * R_x
+
+
+def get_wrist_center(ee_position, ee_orientation):
+    """
+    Return the wrist center for a given ee position and orientation
+    """
+    gripper_position = Matrix(ee_position)
+    r0_6 = create_rotation_matrix(ee_orientation)
+
+    # d7 is distance from joint 6 to gripper
+    displacement = r0_6 * R_corr3 * Matrix([[0], [0], [d7]]).evalf(subs=s)
+    wc_position = gripper_position - displacement
+
+    return wc_position
+
+
+def get_first_three_joints(wrist_pos):
+    """
+    Calculate angles of the first three joints for the given wrist position
+    :param wrist_pos: (x, y, z)
+    :return: (joint1, joint2, joint3)
+    """
+    # project onto xy plane
+    theta1 = atan2(wrist_pos[1], wrist_pos[0])
+
+    def law_of_cosines(a, b, c):
+        """
+        Return angle given lengths of sides a, b, and c
+        """
+        return acos((c ** 2 - a ** 2 - b ** 2) / (-2 * a * b))
+
+    link2 = s[a2]
+    link3 = sqrt(s[a3]**2 + s[d4]**2)
+    # wrist position projected onto plane of arm, minus joint 2 position
+    projected_wc = sqrt(wrist_pos[0]**2 + wrist_pos[1]**2) - s[a1], wrist_pos[2] - s[d1]
+    d_wc = sqrt(projected_wc[0]**2 + projected_wc[1]**2)
+
+    # theta 2 is equal to the triangle angle plus the angle of the wc
+    theta2 = law_of_cosines(link2, d_wc, link3) + atan2(projected_wc[1], projected_wc[0])
+    theta2 = -theta2 + np.pi / 2  # correct for join 2 orientation
+
+    theta3 = - law_of_cosines(link2, link3, d_wc) + np.pi/2
+
+    return theta1, theta2, theta3
+
+
+def get_inverse(position, orientation):
+    """
+    Given the desired position and orientation of the end effector, calculate the required joint angles
+    :param position:
+    :param orientation:
+    :return:
+    """
+    wc = get_wrist_center(position, orientation)
+    theta1, theta2, theta3 = get_first_three_joints(wc)
+
+    r0_3 = (T0_1 * T1_2 * T2_3).evalf(subs={q1: theta1, q2: theta2, q3: theta3})[:3, :3]
+    r0_6 = Transpose(R_corr3) * create_rotation_matrix(orientation)
+    r3_6 = Transpose(r0_3) * r0_6
+    r3_6_sym = simplify(T3_4 * T4_5 * T5_6)
+    theta4, theta5, theta6 = wrist_angles_from_transform(r3_6)
+    return theta1, theta2, theta3, theta4, theta5, theta6
+
+# [
+# [6.12323399573677e-17*sin(q4)*sin(q6) - 7.49879891330929e-33*sin(q4)*cos(q6) - 1.0*sin(q5)*cos(q4) - 7.49879891330929e-33*sin(q6)*cos(q4)*cos(q5) - 6.12323399573677e-17*cos(q4)*cos(q5)*cos(q6),  1.22464679914735e-16*sin(q4)*sin(q6) + 1.0*sin(q4)*cos(q6) + 1.0*sin(q6)*cos(q4)*cos(q5) - 1.22464679914735e-16*cos(q4)*cos(q5)*cos(q6), -1.0*sin(q4)*sin(q6) + 1.22464679914735e-16*sin(q4)*cos(q6) - 6.12323399573677e-17*sin(q5)*cos(q4) + 1.22464679914735e-16*sin(q6)*cos(q4)*cos(q5) + 1.0*cos(q4)*cos(q5)*cos(q6), -0.054],
+# [                                                                                                     -7.49879891330929e-33*sin(q5)*sin(q6) - 6.12323399573677e-17*sin(q5)*cos(q6) + 1.0*cos(q5),                                                                                     (1.0*sin(q6) - 1.22464679914735e-16*cos(q6))*sin(q5),                                                                                       1.22464679914735e-16*sin(q5)*sin(q6) + 1.0*sin(q5)*cos(q6) + 6.12323399573677e-17*cos(q5),    1.5],
+# [1.0*sin(q4)*sin(q5) + 7.49879891330929e-33*sin(q4)*sin(q6)*cos(q5) + 6.12323399573677e-17*sin(q4)*cos(q5)*cos(q6) + 6.12323399573677e-17*sin(q6)*cos(q4) - 7.49879891330929e-33*cos(q4)*cos(q6), -1.0*sin(q4)*sin(q6)*cos(q5) + 1.22464679914735e-16*sin(q4)*cos(q5)*cos(q6) + 1.22464679914735e-16*sin(q6)*cos(q4) + 1.0*cos(q4)*cos(q6),  6.12323399573677e-17*sin(q4)*sin(q5) - 1.22464679914735e-16*sin(q4)*sin(q6)*cos(q5) - 1.0*sin(q4)*cos(q5)*cos(q6) - 1.0*sin(q6)*cos(q4) + 1.22464679914735e-16*cos(q4)*cos(q6),      0],
+# [                                                                                                                                                                                              0,                                                                                                                                        0,                                                                                                                                                                               0,      1]]
+
+# r3_6_blah = [
+#     [- sin(q5) * cos(q4), sin(q4) * cos(q6) + sin(q6) * cos(q4) * cos(q5), - sin(q4) * sin(q6) + cos(q4) * cos(q5) * cos(q6), -0.054],
+#     [cos(q5), 1.0 * sin(q6) * sin(q5), sin(q5) * cos(q6), 1.5],
+#     [sin(q4) * sin(q5), -sin(q4) * sin(q6) * cos(q5) + cos(q4) * cos(q6), - sin(q4) * cos(q5) * cos(q6) - sin(q6) * cos(q4), 0],
+#     [0, 0, 0, 1]]
---
+++ ik_tests/kinematics_test.py
@@ -0,0 +1,104 @@
+import unittest
+import kinematics
+from collections import namedtuple
+
+TestCase = namedtuple('TestCase', 'name ee_pos ee_angles wc_pos joint_angles')
+
+forward_test_cases = {1: [],
+                      2: [[[-0.56754, 0.93663, 3.0038],
+                           [0.62073, 0.48318, 0.38759, 0.480629]],
+                          [-0.638, 0.64198, 2.9988],
+                          [-0.79, -0.11, -2.33, 1.94, 1.14, -3.68]],
+                      3: [[[-1.3863, 0.02074, 0.90986],
+                           [0.01735, -0.2179, 0.9025, 0.371016]],
+                          [-1.1669, -0.17989, 0.85137],
+                          [-2.99, -0.12, 0.94, 4.06, 1.29, -4.12]]}
+test_cases = [
+    TestCase(
+        name='All Zero',
+        ee_pos=[2.153, 0.0, 1.9465],
+        ee_angles=[0, 0, 0],
+        wc_pos=[1.85, 0, 1.946],
+        joint_angles=[0, 0, 0, 0, 0, 0]),
+    TestCase(
+        name='Joint 1',
+        ee_pos=[1.8039, 1.175, 1.9465],
+        ee_angles=[0, 0, .58],
+        wc_pos=[1.55, 1.001, 1.946],
+        joint_angles=[.58, 0, 0, 0, 0, 0]),
+    TestCase(
+        name='Joint 2',
+        ee_pos=[1.4871, 0.0, 2.5909],
+        ee_angles=[0, -0.432, 0],
+        wc_pos=[1.2119, 0, 2.4641],
+        joint_angles=[0, -0.43, 0, 0, 0, 0]),
+    TestCase(
+        name='Random Configuration 1',
+        ee_pos=[2.16135, -1.42635, 1.55109],
+        ee_angles=[1.654, .490, 0.062],
+        wc_pos=[1.89451, -1.44302, 1.69366],
+        joint_angles=[-0.65, 0.45, -0.36, 0.95, 0.79, 0.49]
+    ),
+    TestCase(
+        name='Random Configuration 2',
+        ee_pos=[1.603, -1.011, 1.599],
+        ee_angles=[-1.527, .479, -.937],
+        wc_pos=[1.444, -.794, 1.739],
+        joint_angles=[-.5, -.15, .28, -.81, .54, -.58])
+]
+
+
+class KinematicsTest(unittest.TestCase):
+
+    def assert_position(self, actual, expected, places=2):
+        self.assertAlmostEqual(actual[0], expected[0], places, 'wrong x position')
+        self.assertAlmostEqual(actual[1], expected[1], places, 'wrong y position')
+        self.assertAlmostEqual(actual[2], expected[2], places, 'wrong z position')
+
+    def assert_angles(self, actual, expected, places=2):
+        self.assertAlmostEqual(actual[0], expected[0], places, 'wrong roll angle')
+        self.assertAlmostEqual(actual[1], expected[1], places, 'wrong pitch angle')
+        self.assertAlmostEqual(actual[2], expected[2], places, 'wrong yaw angle')
+
+    def check_effector_pose(self, case):
+        angles = case.joint_angles
+
+        actual_position, actual_orientation = kinematics.get_forward(angles)
+
+        self.assert_position(actual_position, case.ee_pos, places=1)
+        self.assert_angles(actual_orientation, case.ee_angles, places=1)
+
+    def check_wrist_center(self, case):
+        actual_wc = kinematics.get_wrist_center(case.ee_pos, case.ee_angles)
+        self.assert_position(actual_wc, case.wc_pos)
+
+    def test_forward(self):
+        for case in test_cases:
+            with self.subTest(case.name):
+                self.check_effector_pose(case)
+
+    def test_wrist_center(self):
+        for case in test_cases:
+            with self.subTest(case.name):
+                self.check_wrist_center(case)
+
+    def test_inverse(self):
+        for case in test_cases:
+            with self.subTest(case.name):
+                angles = kinematics.get_inverse(case.ee_pos, case.ee_angles)
+                # assert first three angles directly
+                for i in range(0, 3):
+                    self.assertAlmostEqual(angles[i], case.joint_angles[i], 1, "wrong joint {} angle".format(i + 1))
+
+                calculated_pos, calculated_angle = kinematics.get_forward(angles)
+                self.assert_position(calculated_pos, case.ee_pos)
+                self.assert_angles(calculated_angle, case.ee_angles)
+
+    def test_rotation_matrix(self):
+        original_angles = (.5, -.3, .7)
+        matrix = kinematics.create_rotation_matrix(original_angles)
+        new_angles = kinematics.angles_from_transform(matrix)
+
+        self.assertAlmostEqual(new_angles[0], original_angles[0], 5, 'wrong roll angle')
+        self.assertAlmostEqual(new_angles[1], original_angles[1], 5, 'wrong pitch angle')
+        self.assertAlmostEqual(new_angles[2], original_angles[2], 5, 'wrong yaw angle')
